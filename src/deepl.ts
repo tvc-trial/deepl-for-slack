c5f33bd28b313f0acbeaa7e33cace7a03fb0fbad
Yoshihiro-Hirose <Yoshihiro_Hirose@sis2.saison.co.jp>
2020-10-22 14:56:13 +0900

    DeepL 呼び出しをモックに置き換え

--- src/deepl.ts
+++ src/deepl.ts
@@ -9,33 +9,37 @@ export class DeepLApi {
   constructor(authKey: string, logger: Logger) {
     this.authKey = authKey;
     this.logger = logger;
-    this.axiosInstance = axios.create({
-      baseURL: 'https://api.deepl.com/v2',
-      timeout: 30000,
-    });
+    // 一時機能無効
+    // this.axiosInstance = axios.create({
+    //   baseURL: 'https://api.deepl.com/v2',
+    //   timeout: 30000,
+    // });
   }
 
   async translate(text: string, targetLanguage: string): Promise<string | null> {
-    return this.axiosInstance({
-      url: "/translate",
-      data: qs.stringify({
-        auth_key: this.authKey,
-        text: text,
-        target_lang: targetLanguage.toUpperCase()
-      }),
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8"
-      }
-    }).then(response => {
-      this.logger.debug(response.data);
-      if (response.data.translations && response.data.translations.length > 0) {
-        return response.data.translations[0].text;
-      } else {
-        return ":x: Failed to translate it due to an unexpected response from DeepL API";
-      }
-    }).catch(error => {
-      this.logger.error(`Failed to translate - text: ${text} error: ${error}`);
-      return `:x: Failed to translate it due to ${error}`;
-    });
+    return Promise.resolve('★ダミーエコーレスポンス\n'+ text);
+
+    // 一時機能無効
+    // return this.axiosInstance({
+    //   url: "/translate",
+    //   data: qs.stringify({
+    //     auth_key: this.authKey,
+    //     text: text,
+    //     target_lang: targetLanguage.toUpperCase()
+    //   }),
+    //   headers: {
+    //     "content-type": "application/x-www-form-urlencoded;charset=utf-8"
+    //   }
+    // }).then(response => {
+    //   this.logger.debug(response.data);
+    //   if (response.data.translations && response.data.translations.length > 0) {
+    //     return response.data.translations[0].text;
+    //   } else {
+    //     return ":x: Failed to translate it due to an unexpected response from DeepL API";
+    //   }
+    // }).catch(error => {
+    //   this.logger.error(`Failed to translate - text: ${text} error: ${error}`);
+    //   return `:x: Failed to translate it due to ${error}`;
+    // });
   }
 }
